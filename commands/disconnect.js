85db14c60773929ef5537ba0e701089f5e08a075
Muhammad Fahad <muhfahad960@gmail.com>
2024-11-27 16:34:14 +0500

    added disconnection slash command

---
+++ commands/disconnect.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('disconnect')
+        .setDescription('Disconnect from the voice channel.'),
+
+    async execute({client, interaction}) {
+        await interaction.deferReply();
+        let queue = client.player.nodes.get(interaction.guild);
+
+        if (!queue.connection) return interaction.editReply(`Not connected to any voice channel! ❌`);
+        const success = queue.disconnect(interaction.member.voice.channel);
+
+        if (success) return interaction.editReply(`Left the voice channel. ✅`);
+    }
+}
